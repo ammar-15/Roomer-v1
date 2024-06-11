40733084c380520a9cfd3ca489b8ea32a96e0b6c
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-06-10 22:22:40 -0230

    first steps

---
+++ index.js
@@ -0,0 +1,15 @@
+const div = document.querySelector("div")
+const date = document.querySelector(".date")
+const dailyData = document.querySelector(".dailyData")
+const inHouse = document.querySelector(".inHouse")
+const stayovers = document.querySelector(".stayovers")
+const checkouts = document.querySelector(".checkouts")
+
+
+function displayDate() {
+    
+}
+
+function main() {
+    displayDate();
+}
