1defa665f2ffd560fedce6bc3a18750f6ffad4e9
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-07-02 20:31:11 -0230

    live date and time

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-const div = document.querySelector("div")
-const date = document.querySelector(".date")
-const timeElement = document.querySelector(".time")
-const dailyData = document.querySelector(".dailyData")
-const inHouse = document.querySelector(".inHouse")
-const stayovers = document.querySelector(".stayovers")
-const checkouts = document.querySelector(".checkouts")
+// DATE AND TIME //
+const dateElement = document.querySelector(".date");
+const timeElement = document.querySelector(".time");
 
+/** 
+* @param {Date} date
+*/
+function formatTime(date) {
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const isAM = date.getHours() < 12;
 
-function displayDate() {
-    
+    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAM ? "AM" : "PM"}`; 
 }
 
-function main() {
-    displayDate();
+function formatDate(date) {
+    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    date.getDay()
+    return`${DAYS[date.getDay()]}, ${date.getDate()} ${MONTHS[date.getMonth()]} ${date.getFullYear()}`;
 }
+
+setInterval(() => {
+    const now = new Date();
+    timeElement.textContent = formatTime(now);
+    dateElement.textContent = formatDate(now);
+}, 200)
