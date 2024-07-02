afbc04e8dabd1e07dc3ffa217f3dd9e0aff3ae79
ammar-15 <71677758+ammar-15@users.noreply.github.com>
2024-07-02 19:27:11 -0230

    font change

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const div = document.querySelector("div")
 const date = document.querySelector(".date")
+const timeElement = document.querySelector(".time")
 const dailyData = document.querySelector(".dailyData")
 const inHouse = document.querySelector(".inHouse")
 const stayovers = document.querySelector(".stayovers")
